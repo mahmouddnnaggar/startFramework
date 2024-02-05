aadfd34f7989e4be94a1472396b6f4ec7d98370a
Mahmoud <mahmouddnnaggar@gmail.com>
2024-02-06 01:43:34 +0200

    second

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StartComponent } from './start/start.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'start', pathMatch: 'full'},
+  {path: 'start', component:StartComponent, title: 'Start framewor'},
+  {path: 'about', component:AboutComponent, title: 'About'},
+  {path: 'contact', component:ContactComponent, title: 'Contact'},
+  {path: 'portfolio', component:PortfolioComponent, title: 'Portfolio'},
+  {path: '**', component:NotFoundComponent, title: 'Not found'},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartComponent } from './start/start.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    AboutComponent,
+    PortfolioComponent,
+    ContactComponent,
+    NotFoundComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
